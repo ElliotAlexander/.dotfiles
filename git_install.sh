014a012e99e152506fe26375a490cfcee407df6c
ElliotAlexander <ep1e16@soton.ac.uk>
2018-08-07 12:55:38 +0100

    Adding macos setup script

--- git_install.sh
+++ git_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+GPG_PUBLIC = 5A61A1B55914A21A718B7E114D975740C41F7630;
+
 sudo apt-get install git
 
 # git-config
@@ -7,11 +9,11 @@ git config --global user.email "ep1e16@soton.ac.uk"
 git config --global user.name "ElliotAlexander"
 git config --global core.editor "vim"
 
-if gpg --list-keys 626BE6CD; then
+if gpg --list-keys $GPG_PUBLIC; then
 	# Signing commits using GPG
-	git config --global user.signingkey 5A61A1B55914A21A718B7E114D975740C41F7630
+	git config --global user.signingkey $GPG_PUBLIC
 	# To set all commits to be signed by defaul
 	git config --global commit.gpgsign true
 else
-	echo "GPG Key 5A61A1B55914A21A718B7E114D975740C41F7630 not found."
+	echo "GPG Key $GPG_PUBLIC not found."
 fi
