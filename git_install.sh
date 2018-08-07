dadec882a4b3936538bea0ecf53fbcb5d0e06d64
ElliotAlexander <ep1e16@soton.ac.uk>
2018-08-07 12:57:12 +0100

    Added git credential store timeout

--- git_install.sh
+++ git_install.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-GPG_PUBLIC = 5A61A1B55914A21A718B7E114D975740C41F7630;
+GPG_PUBLIC=5A61A1B55914A21A718B7E114D975740C41F7630;
 
 sudo apt-get install git
 
 # git-config
+
+#Set credential store to save passswords for an hour.
+git config credential.helper store
 git config --global user.email "ep1e16@soton.ac.uk"
 git config --global user.name "ElliotAlexander"
 git config --global core.editor "vim"
+git config --global credential.helper 'cache --timeout=3600'
+
 
 if gpg --list-keys $GPG_PUBLIC; then
 	# Signing commits using GPG
