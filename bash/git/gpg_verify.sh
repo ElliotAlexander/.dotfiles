bfd78f09d0b2d3e7361e2862b760094dfa04a518
Elliot Alexander <ep1e16@soton.ac.uk>
2019-08-04 12:10:37 +0100

    Refactored folder structure

--- bash/git/gpg_verify.sh
+++ bash/git/gpg_verify.sh
@@ -6,11 +6,13 @@ while getopts ":sv" opt; do
   case ${opt} in
     v )
       SIG=$2
+      Using signature file $2
       FILE=$3
       gpg --no-options --keyid-format 0xlong --verify $SIG $FILE
       ;;
     s )
       KEY=$2
+      echo Using key file $2
       gpg --import $2
       ;;
     \? ) echo "Usage: cmd [-s] [-v]"
