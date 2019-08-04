9635f2e3b03fd247417d08f143e03ea31ee8a02e
Elliot Alexander <ep1e16@soton.ac.uk>
2019-08-04 23:35:14 +0100

    Removing git install and update scripts.

--- bash/git/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Copying..."
-cp  -v ~/$1 .
-git add *
-# Explicitly add this - git notmally ignores .* files in .git add
-git add $1
-git commit -am "Updated $1"
-git push origin master
