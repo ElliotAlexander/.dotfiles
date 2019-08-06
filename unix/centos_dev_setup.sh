07bbcdaf2473e9494f71c0712b34d5333d0cd673
Elliot Alexander <elliot@netcraft.com>
2019-08-06 12:27:37 +0100

    Added Node and docker to setup script.

--- unix/centos_dev_setup.sh
+++ unix/centos_dev_setup.sh
@@ -22,5 +22,31 @@ echo "ZSH installed."
 
 wget -O bat.zip https://github.com/sharkdp/bat/releases/download/v0.7.1/bat-v0.7.1-x86_64-unknown-linux-musl.tar.gz
 tar -xvzf bat.zip -C /usr/local
-cd /usr/local && mv bat-v0.7.1-x86_64-unknown-linux-musl bat
-cd ~
+cd /usr/local && sudo mv bat-v0.7.1-x86_64-unknown-linux-musl bat
+
+
+# Setup Docker service
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce
+sudo usermod -aG docker $(whoami)
+
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+
+
+# Setup docker-compose
+sudo yum install epel-release
+sudo yum install -y python-pip
+sudo pip install docker-compose
+sudo yum upgrade python*
+
+
+# Setup node / npm
+sudo yum install npm
+sudo npm install -g yarn
+
+# Update to latest
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stable
