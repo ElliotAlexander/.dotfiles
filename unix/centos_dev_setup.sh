d318fe2b1cb3b5be12eb1588584bf436cca7cbfe
Elliot Alexander <elliot@netcraft.com>
2019-08-06 12:34:41 +0100

    Adding node to CentOS dev script

--- unix/centos_dev_setup.sh
+++ unix/centos_dev_setup.sh
@@ -28,7 +28,7 @@ cd /usr/local && sudo mv bat-v0.7.1-x86_64-unknown-linux-musl bat
 # Setup Docker service
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install docker-ce
+sudo yum install -y docker-ce
 sudo usermod -aG docker $(whoami)
 
 sudo systemctl enable docker.service
@@ -36,17 +36,17 @@ sudo systemctl start docker.service
 
 
 # Setup docker-compose
-sudo yum install epel-release
+sudo yum install -yepel-release
 sudo yum install -y python-pip
-sudo pip install docker-compose
+sudo pip install -y docker-compose
 sudo yum upgrade python*
 
 
 # Setup node / npm
-sudo yum install npm
-sudo npm install -g yarn
+sudo yum install -y npm
+sudo npm install -yg yarn
 
 # Update to latest
 sudo npm cache clean -f
-sudo npm install -g n
+sudo npm install -yg n
 sudo n stable
