c75af64fab32dcbd303e17e098ed061ef0ec1edf
Elliot Alexander <ep1e16@soton.ac.uk>
2019-08-25 00:42:40 +0100

    Added centos provisioning scripts

---
+++ unix/centos_provision_setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Setting up ssh"
+mkdir ~/.ssh
+touch ~/.ssh/authorized_keys
+
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/authorized_keys
+chmod -R go= ~/.ssh
+
+cp ./sshd_config /etc/ssh/sshd_config
+
+yum update && yum install policycoreutils-python
+
+semanage port -a -t ssh_port_t -p tcp 6022
+firewall-cmd --add-port=6022/tcp --permanent
+firewall-cmd --reload
+
+
+sudo systemctl restart sshd.service
