d71c47498e00dc7ea5d6599a13d73fd7569f1dbd
Elliot Alexander <ep1e16@soton.ac.uk>
2019-08-25 00:57:16 +0100

    Added public key.

--- unix/centos_provision_setup.sh
+++ unix/centos_provision_setup.sh
@@ -2,15 +2,16 @@
 
 echo "Setting up ssh"
 mkdir ~/.ssh
-touch ~/.ssh/authorized_keys
+cat ../id.pub | ~/.ssh/authorized_keys
 
 chmod 700 ~/.ssh
 chmod 600 ~/.ssh/authorized_keys
 chmod -R go= ~/.ssh
 
+
 cp ./sshd_config /etc/ssh/sshd_config
 
-yum update && yum install policycoreutils-python
+yum update && yum install -y policycoreutils-python
 
 semanage port -a -t ssh_port_t -p tcp 6022
 firewall-cmd --add-port=6022/tcp --permanent
