5da4c53b11ca7626eef6b24ce25c7d1be221c7d9
Elliot Alexander <ep1e16@soton.ac.uk>
2019-08-25 01:05:38 +0100

    Fixed permissions issue in provisioning script

--- unix/centos_provision_setup.sh
+++ unix/centos_provision_setup.sh
@@ -2,20 +2,20 @@
 
 echo "Setting up ssh"
 mkdir ~/.ssh
-cat ../id.pub | ~/.ssh/authorized_keys
+cat ../id.pub > ~/.ssh/authorized_keys
 
 chmod 700 ~/.ssh
 chmod 600 ~/.ssh/authorized_keys
-chmod -R go= ~/.ssh
+sudo chmod -R go= ~/.ssh
 
 
-cp ./sshd_config /etc/ssh/sshd_config
+sudo cp ./sshd_config /etc/ssh/sshd_config
 
-yum update && yum install -y policycoreutils-python
+sudo yum update && yum install -y policycoreutils-python
 
-semanage port -a -t ssh_port_t -p tcp 6022
-firewall-cmd --add-port=6022/tcp --permanent
-firewall-cmd --reload
+sudo semanage port -a -t ssh_port_t -p tcp 6022
+sudo firewall-cmd --add-port=6022/tcp --permanent
+sudo firewall-cmd --reload
 
 
 sudo systemctl restart sshd.service
