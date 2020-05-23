c4b6623027983d42347337b08c800ecbad1704ac
ElliotAlexander <elliot.alex.97@gmail.com>
2020-05-23 12:24:59 +0100

    Removed old files

--- MacOS/README.md
+++
@@ -1,3 +0,0 @@
-# .dotfiles
-
-Most of this is installation / setup scripts, or config files for various environments. 
--- MacOS/ssh/sshgen.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-read -p "Enter a password: "  password
-read -p "Enter a comment: "  comment
-ssh-keygen -t rsa -b 4096 -P $password -C $comment
-echo "Done. "
