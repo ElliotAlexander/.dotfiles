6dc98889c0c027e890eff5e417ec13f139c39218
Elliot Alexander <gpg@mail.elliotalexander.uk>
2022-04-20 12:22:15 +0100

    Add brew to scripts

--- ssh/sshcopy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-read -p "Enter a hostname: " hostname
-read -p "Enter your username: " username
-
-echo "Attempting to copy SSH key..."
-ssh-copy-id username@hostname
